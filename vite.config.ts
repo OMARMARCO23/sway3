63cf5eca7a502c345603a576ac78c83ef42226ee
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-09-23 22:53:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace this with your Vercel deployment URL
-const VERCEL_BACKEND = "https://sway3.vercel.app";
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: VERCEL_BACKEND,
-        changeOrigin: true,
-      },
-    },
+  optimizeDeps: {
+    include: ["tesseract.js"]
   },
+  build: {
+    rollupOptions: {
+      external: [], // you can add "tesseract.js" here if bundling still breaks
+    }
+  }
 });
