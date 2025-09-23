fd88537f5bb5d7c088496c3139189f8dadd8362f
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-09-24 00:25:40 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext"; // ✅ add this
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
