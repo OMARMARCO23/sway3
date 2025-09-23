cc723677f42f628e21807d9332f7d6f03da5e642
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-09-24 00:25:14 +0100

    Create LanguageContext.tsx

---
+++ src/contexts/LanguageContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+type Language = "en" | "fr" | "ar";
+
+interface LanguageContextType {
+  language: Language;
+  setLanguage: (lang: Language) => void;
+}
+
+const LanguageContext = createContext<LanguageContextType>({
+  language: "en",
+  setLanguage: () => {},
+});
+
+export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [language, setLanguage] = useState<Language>(
+    (localStorage.getItem("language") as Language) || "en"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("language", language);
+  }, [language]);
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
